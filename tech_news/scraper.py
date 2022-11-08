e32caae8c931518a1580d4890c67ca4cd39472b9
dedojs <xandresousax@gmail.com>
2022-11-08 16:20:35 -0300

    feat: scrape_novidades

--- tech_news/scraper.py
+++ tech_news/scraper.py
@@ -1,5 +1,6 @@
 import requests
 import time
+from parsel import Selector
 
 
 # Requisito 1
@@ -19,7 +20,9 @@ def fetch(url):
 
 # Requisito 2
 def scrape_novidades(html_content):
-    """Seu código deve vir aqui"""
+    selector = Selector(text=html_content)
+    news_link = selector.css('div.cs-overlay a::attr(href)').getall()
+    return news_link
 
 
 # Requisito 3
