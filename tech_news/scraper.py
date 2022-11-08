b5fbbf5b62982838d5ae3f45ca05241ac1213978
dedojs <xandresousax@gmail.com>
2022-11-08 16:27:16 -0300

    feat: scrape_next_page_link

--- tech_news/scraper.py
+++ tech_news/scraper.py
@@ -27,7 +27,9 @@ def scrape_novidades(html_content):
 
 # Requisito 3
 def scrape_next_page_link(html_content):
-    """Seu código deve vir aqui"""
+    selector = Selector(text=html_content)
+    new_page = selector.css('a.next.page-numbers::attr(href)').get()
+    return new_page
 
 
 # Requisito 4
