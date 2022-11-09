69f8a876ef47fdb45d21ba1a877088b18324ecae
dedojs <xandresousax@gmail.com>
2022-11-09 18:53:32 -0300

    feat: top_5_news

--- tech_news/analyzer/ratings.py
+++ tech_news/analyzer/ratings.py
@@ -1,6 +1,14 @@
+from tech_news.database import db
+
+
 # Requisito 10
 def top_5_news():
-    """Seu código deve vir aqui"""
+    result_order = db.news.find().sort('comments_count', -1)
+    result = []
+    for new in result_order:
+        result.append((new['title'], new['url']))
+
+    return result[:5]
 
 
 # Requisito 11
